10c2303aae6ddd4cbe8cd9edcf0805cdde7a43b8
Kiran Madabhushi <kiran@valtix.com>
2022-03-17 01:02:24 -0700

    add subnets to output

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,14 @@ output "vpc_id" {
   value = google_compute_network.datapath_network.self_link
 }
 
+output "mgmt_subnet_id" {
+  value = google_compute_subnetwork.mgmt_subnet.self_link
+}
+
+output "datapath_subnet_id" {
+  value = google_compute_subnetwork.datapath_subnet.self_link
+}
+
 output "mgmt_security_group" {
   value = tolist(google_compute_firewall.mgmt_ingress.target_tags)[0]
 }
